b53c359006fce23b3600a78b5b518147cb159318
Emanuela <ebargelesi@gmail.com>
2019-07-24 17:12:40 +0200

    Aggiornato l'esercizio con la grafica passata da Simone. Aggiunta una bozza del search. Aggiunto cambio classe quando si seleziona un contatto.

--- main.js
+++ main.js
@@ -2,18 +2,42 @@ $(document).ready(function() {
 
 
 
-
-  $('#submitmsg')
+//botta e risposta tra utente e cpu
+  $('.fa-microphone')
     .on('click', function() {
-      var message = $('#inpututente').val();
-      $('#schermochatutente').append('<p class="paroleutente">' + message + '</p>');
-      $('#inpututente').val('');
+      var message = $('.new-message-inputs').val();
+      $('.right-messages').append('<p class = "greenbubble">' + message + '</p>');
+      $('.new-message-inputs').val('');
 serverResponse(message);
     });
-});
-
 
 
 function serverResponse(message) {
-  $('#schermochatcpu').append('<p class="rispostacpu"> Risposta: ' + 'OK!' + '</p>');
+  $('.right-messages').append('<p class = "graybubble"> Risposta: ' + 'OK!' + '</p>');
 }
+
+
+//filtro i contatti
+  $("#contacts-filter").on("keyup", function() {
+    var value = $(this).val().toLowerCase();
+    $(".contacts *").filter(function() {
+      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+    });
+  });
+
+
+// rimuovo classe active da tutti i contatti ogni volta che la imposto su uno per selezionarlo
+$(".contact").on('click', function() {
+  if($(".contact").hasClass('active')){
+     $(".contact").removeClass('active');
+   }
+  $(this).addClass("active");
+});
+
+//giusto per provare a cambiare il testo nel DOM
+$(".left-disclaimer-text a").on('click', function() {
+  $(".left-disclaimer-text a").html("Disattiva notifiche desktop");
+});
+
+
+});
