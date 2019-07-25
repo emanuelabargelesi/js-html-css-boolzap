8c432827903d6e8b211a50be6e4914b82ea749e4
Emanuela <ebargelesi@gmail.com>
2019-07-25 17:01:17 +0200

    Aggiunto "toggle" delle notifiche sul click Link tra chat e utente (da finire) Aggiunto "sta scrivendo" vicino all'immagine dell'utente

--- main.js
+++ main.js
@@ -1,23 +1,38 @@
 $(document).ready(function() {
 
 
+  // $('.new-message-inputs')
+  // .on('click', function() {
+  //   $('.fa-microphone').toggle();
+  //   $('.fa-paper-plane').toggle();
+  // });
 
-//botta e risposta tra utente e cpu
-  $('.fa-microphone')
-    .on('click', function() {
-      var message = $('.new-message-inputs').val();
-      $('.right-messages').append('<p class = "greenbubble">' + message + '</p>');
-      $('.new-message-inputs').val('');
-serverResponse(message);
+  //togglie immagine quando entri nell'imput
+  $('.new-message-inputs')
+    .mouseenter(function() {
+      $('.fa-microphone').hide();
+      $('.fa-paper-plane').show();
+    }).mouseleave(function() {
+      $('.fa-microphone').show();
+      $('.fa-paper-plane').hide();
     });
 
 
-function serverResponse(message) {
-  $('.right-messages').append('<p class = "graybubble"> Risposta: ' + 'OK!' + '</p>');
-}
+
+  //botta e risposta tra utente e cpu
+  $('.fa-microphone').on('click', function() {
+    var message = $('.new-message-inputs').val();
+    $('.right-messages').append('<p class = "greenbubble">' + message + '</p>');
+    $('.new-message-inputs').val('');
+    serverResponse(message);
+  });
+
+  function serverResponse(message) {
+    $('.right-messages').append('<p class = "graybubble"> Risposta: ' + 'OK!' + '</p>');
+  }
 
 
-//filtro i contatti
+  //filtro i contatti
   $("#contacts-filter").on("keyup", function() {
     var value = $(this).val().toLowerCase();
     $(".contacts *").filter(function() {
@@ -26,18 +41,36 @@ function serverResponse(message) {
   });
 
 
-// rimuovo classe active da tutti i contatti ogni volta che la imposto su uno per selezionarlo
-$(".contact").on('click', function() {
-  if($(".contact").hasClass('active')){
-     $(".contact").removeClass('active');
-   }
-  $(this).addClass("active");
-});
+  // rimuovo classe active da tutti i contatti ogni volta che la imposto su uno per selezionarlo e gli collego una chat
+  $(".contact").on('click', function() {
+    var refchat = $(this).attr('refchat');
+    if ($(".contact.active")) { //.contact.active, senza spazi, va a prendere solo quelli con class active (con lo spazio i figli)
+      $(".contact").removeClass('active');
+      $(".right-messages").removeClass('active');
+    }
+    $(this).addClass("active");
+    $('.right-messages[refchat="' + refchat + '"]').addClass('active');
+    // console.log('il valore di refchat è ' + refchat);
+  });
 
-//giusto per provare a cambiare il testo nel DOM
-$(".left-disclaimer-text a").on('click', function() {
-  $(".left-disclaimer-text a").html("Disattiva notifiche desktop");
-});
+
+//attivare e disattivare le notifiche
+  $('.left-disclaimer-text a').on('click', function() {
+    $(this).text(function(i, text){
+          return text === 'Attiva notifiche desktop' ? 'Disattiva notifiche desktop' : 'Attiva notifiche desktop';
+      });
+  });
+
+
+
+  //sta scrivendo
+  $(".new-message-inputs").on('click', function() {
+    $(".stascrivendo").html('<p> Sta scrivendo ...</p>')
+      .css({
+        'font-size': '12px',
+        'align-items': 'flex-end'
+      });
+  });
 
 
 });
